1990534d022bea0679dfb1e2a90122fdcdd48174
oluwaseyimokuolu <95516055+oluwaseyimokuolu@users.noreply.github.com>
2021-12-13 08:48:23 -0500

    Update on ec2
    Changes were made to the ec2 type

--- main/ec2.tf
+++ main/ec2.tf
@@ -1,6 +1,7 @@
+# This gives the details about the ec2 instance (server)
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
